a3a2992036a8eec0229b8485b7c905c608a65e18
Al Sweigart <asweigart@gmail.com>
2018-08-28 22:48:03 -0700

    Implemented the backend code for color (though it's still not working). Added copy() and paste() methods.

--- pytextcanvas/__init__.py
+++ pytextcanvas/__init__.py
@@ -38,7 +38,7 @@ Road Map of Features:
 
 # TODO - add a mode where drawing ooutside the canvas is a no-op instead of raising an exception.
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 import doctest
 import math
@@ -52,6 +52,7 @@ DEFAULT_CANVAS_WIDTH = 80
 DEFAULT_CANVAS_HEIGHT = 25
 
 
+# TODO - Separate out htis color stuff and hexColor() to their own module. Research how colorama and other modules use color.
 # Based off of the CSS3 standard names. Data from James Bennett's webcolors module: https://github.com/ubernostrum/webcolors
 COLOR_NAMES_TO_HEX = {'aliceblue': '#f0f8ff','antiquewhite': '#faebd7','aqua': '#00ffff','aquamarine': '#7fffd4','azure': '#f0ffff','beige': '#f5f5dc','bisque': '#ffe4c4','black': '#000000','blanchedalmond': '#ffebcd','blue': '#0000ff','blueviolet': '#8a2be2','brown': '#a52a2a','burlywood': '#deb887','cadetblue': '#5f9ea0','chartreuse': '#7fff00','chocolate': '#d2691e','coral': '#ff7f50','cornflowerblue': '#6495ed','cornsilk': '#fff8dc','crimson': '#dc143c','cyan': '#00ffff','darkblue': '#00008b','darkcyan': '#008b8b','darkgoldenrod': '#b8860b','darkgray': '#a9a9a9','darkgrey': '#a9a9a9','darkgreen': '#006400','darkkhaki': '#bdb76b','darkmagenta': '#8b008b','darkolivegreen': '#556b2f','darkorange': '#ff8c00','darkorchid': '#9932cc','darkred': '#8b0000','darksalmon': '#e9967a','darkseagreen': '#8fbc8f','darkslateblue': '#483d8b','darkslategray': '#2f4f4f','darkslategrey': '#2f4f4f','darkturquoise': '#00ced1','darkviolet': '#9400d3','deeppink': '#ff1493','deepskyblue': '#00bfff','dimgray': '#696969','dimgrey': '#696969','dodgerblue': '#1e90ff','firebrick': '#b22222','floralwhite': '#fffaf0','forestgreen': '#228b22','fuchsia': '#ff00ff','gainsboro': '#dcdcdc','ghostwhite': '#f8f8ff','gold': '#ffd700','goldenrod': '#daa520','gray': '#808080','grey': '#808080','green': '#008000','greenyellow': '#adff2f','honeydew': '#f0fff0','hotpink': '#ff69b4','indianred': '#cd5c5c','indigo': '#4b0082','ivory': '#fffff0','khaki': '#f0e68c','lavender': '#e6e6fa','lavenderblush': '#fff0f5','lawngreen': '#7cfc00','lemonchiffon': '#fffacd','lightblue': '#add8e6','lightcoral': '#f08080','lightcyan': '#e0ffff','lightgoldenrodyellow': '#fafad2','lightgray': '#d3d3d3','lightgrey': '#d3d3d3','lightgreen': '#90ee90','lightpink': '#ffb6c1','lightsalmon': '#ffa07a','lightseagreen': '#20b2aa','lightskyblue': '#87cefa','lightslategray': '#778899','lightslategrey': '#778899','lightsteelblue': '#b0c4de','lightyellow': '#ffffe0','lime': '#00ff00','limegreen': '#32cd32','linen': '#faf0e6','magenta': '#ff00ff','maroon': '#800000','mediumaquamarine': '#66cdaa','mediumblue': '#0000cd','mediumorchid': '#ba55d3','mediumpurple': '#9370db','mediumseagreen': '#3cb371','mediumslateblue': '#7b68ee','mediumspringgreen': '#00fa9a','mediumturquoise': '#48d1cc','mediumvioletred': '#c71585','midnightblue': '#191970','mintcream': '#f5fffa','mistyrose': '#ffe4e1','moccasin': '#ffe4b5','navajowhite': '#ffdead','navy': '#000080','oldlace': '#fdf5e6','olive': '#808000','olivedrab': '#6b8e23','orange': '#ffa500','orangered': '#ff4500','orchid': '#da70d6','palegoldenrod': '#eee8aa','palegreen': '#98fb98','paleturquoise': '#afeeee','palevioletred': '#db7093','papayawhip': '#ffefd5','peachpuff': '#ffdab9','per': '#cd853f','pink': '#ffc0cb','plum': '#dda0dd','powderblue': '#b0e0e6','purple': '#800080','red': '#ff0000','rosybrown': '#bc8f8f','royalblue': '#4169e1','saddlebrown': '#8b4513','salmon': '#fa8072','sandybrown': '#f4a460','seagreen': '#2e8b57','seashell': '#fff5ee','sienna': '#a0522d','silver': '#c0c0c0','skyblue': '#87ceeb','slateblue': '#6a5acd','slategray': '#708090','slategrey': '#708090','snow': '#fffafa','springgreen': '#00ff7f','steelblue': '#4682b4','tan': '#d2b48c','teal': '#008080','thistle': '#d8bfd8','tomato': '#ff6347','turquoise': '#40e0d0','violet': '#ee82ee','wheat': '#f5deb3','white': '#ffffff','whitesmoke': '#f5f5f5','yellow': '#ffff00','yellowgreen': '#9acd32',}
 
@@ -75,7 +76,7 @@ def hexColor(color):
     '#aabbcc'
     """
     if type(color) != str:
-        raise TypeError('Parameter `color` must be of type str, not %s.' % (type(color)))
+        raise PyTextCanvasException('Color must be of type str, not %s.' % (type(color)))
 
     color = color.lower() # normalize to lowercase
 
@@ -92,9 +93,9 @@ def hexColor(color):
         elif len(color) == 6:
             return '#' + color
         else:
-            raise ValueError('Parameter `color` must be a hexadecimal number or valid color name.')
+            raise PyTextCanvasException('Color must be a hexadecimal number or valid color name.')
     except ValueError:
-        raise ValueError('Parameter `color` must be a hexadecimal number, not %s.' % (type(color)))
+        raise PyTextCanvasException('Color` must be a hexadecimal number, not %s.' % (type(color)))
 
 
 def _checkForIntOrFloat(arg):
@@ -177,7 +178,7 @@ def printCharGrid(charGrid):
 '''
 
 class Canvas:
-    def __init__(self, width=None, height=None, loads=None, fg='#000000', bg='#ffffff'):
+    def __init__(self, width=None, height=None, loads=None, defaultFg='#000000', defaultBg='#ffffff'):
         """
         Initialize a new Canvas, which represents a rectangular area of
         text characters. The coordinates start in the upper left corner at
@@ -239,15 +240,19 @@ class Canvas:
         # value represents the default canvas setting. If both fg and bg
         # are set to None, this canvas isn't "colorfied" and doesn't have
         # color information stored (to save memory).
-        if fg is None and bg is None:
-            self.colorfied = False
-            # defaultFg, defaultBg, fg, and bg remain uninitialized.
+        if defaultFg is None and defaultBg is None:
+            self._colorfied = False
+            self._defaultFg = None
+            self._defaultBg = None
+            self._fg = None
+            self._bg = None
         else:
-            self.colorfied = True
-            self.defaultFg = fg
-            self.defaultBg = bg
-            self.fg = [[None] * self._height for i in range(self._width)]
-            self.bg = [[None] * self._height for i in range(self._width)]
+            self._colorfied = True
+            # Default fg and bg are used when a cell has no color info (i.e. None)
+            self._defaultFg = defaultFg
+            self._defaultBg = defaultBg
+            self._fg = [[None] * self._height for i in range(self._width)]
+            self._bg = [[None] * self._height for i in range(self._width)]
         # TODO - the rest of the color implementation needs to be done.
 
         self._cursor = (0, 0) # The cursor is always set to integers.
@@ -294,7 +299,7 @@ class Canvas:
 
         >>> canvas = Canvas(10, 5)
         >>> canvas.size
-        (10, 55)
+        (10, 5)
         """
         return (self._width, self._height)
 
@@ -313,8 +318,59 @@ class Canvas:
 
 
     @property
-    def cursor(self):
+    def colorfied(self):
+        return self._colorfied
 
+    @colorfied.setter
+    def colorfied(self, value):
+        if not self._colorfied and value:
+            # Going from not colorfied to colorfied.
+            self._defaultFg = '#000000'
+            self._defaultBg = '#ffffff'
+            self._fg = [[None] * self._height for i in range(self._width)]
+            self._bg = [[None] * self._height for i in range(self._width)]
+        elif self._colorfied and not value:
+            # Going from colorfied to not colorfied.
+            self._defaultFg = None
+            self._defaultBg = None
+            self._fg = None
+            self._bg = None
+
+        self._colorfied = bool(value)
+
+
+    @property
+    def defaultFg(self):
+        """
+        The default color to use when a position on the canvas has no
+        foreground color set.
+        """
+        return self._cursor
+
+    @defaultFg.setter
+    def defaultFg(self, value):
+        self._defaultFg = hexColor(value)
+
+
+    @property
+    def defaultBg(self):
+        """
+        The default color to use when a position on the canvas has no
+        background color set.
+        """
+        return self._cursor
+
+    @defaultBg.setter
+    def defaultBg(self, value):
+        self._defaultBg = hexColor(value)
+
+
+    @property
+    def cursor(self):
+        """
+        The cursor location as an (x, y) tuple. The cursor is where
+        text is written from in the `write()` method.
+        """
         return self._cursor
 
     @cursor.setter
@@ -800,27 +856,64 @@ class Canvas:
         cells are transparent, not blank. To make the cells blank, call `fill(' ')`."""
         self.fill(None)
 
-    '''
+
     def copy(self, left=0, top=0, width=None, height=None):
+        """
+        Returns a new Canvas object created from a subregion of this Canvas object.
+
+        This returned Canvas can be "pasted" to other Canvas objects using `paste()`.
+        """
         if width is None:
-            width = self.width
+            width = self.width # By default, use the entire width of the canvas.
         if height is None:
-            height = self.height
+            height = self.height # By default, use the entire height of the canvas.
 
-        if (left, top, width, height) == (0, 0, self.width, self.height):
-            # Copy the entire area, we'll just wrap __copy__().
-            return self.__copy__()
-
+        # Create the new Canvas object for the copied data.
         canvasCopy = Canvas(width=width, height=height)
+
+        # Copy the character, fg color, and bg color data.
         for x in range(width):
             for y in range(height):
-                canvasCopy[x, y] = self[x + left] # LEFT OFF
+                canvasCopy._chars[x][y] = self._chars[x + left][y + top]
+                canvasCopy._fg[x][y] = self._fg[x + left][y + top]
+                canvasCopy._bg[x][y] = self._bg[x + left][y + top]
 
-        # TODO - finish
+        # Copy the various properties.
+        canvasCopy._truncate = self._truncate
+        canvasCopy._cursor = self._cursor
+        canvasCopy._colorfied = self._colorfied
+        canvasCopy._defaultFg = self._defaultFg
+        canvasCopy._defaultBg = self._defaultBg
+
+        return canvasCopy
+
 
     def __copy__(self):
-        pass
-    '''
+        return self.copy(0, 0, self.width, self.height)
+
+
+    def paste(self, canvasToPaste, left, top, truncate=True):
+        """
+        Paste the character, foreground color, and background color data from
+        `canvasToPaste` to this `Canvas` object.
+
+        If `truncate` is `False`, raise a `PyTextCanvasException` if any characters
+        are pasted beyond the edges of this `Canvas` object.
+        """
+        if self._truncate: # The truncate setting overrides this truncate argument.
+            truncate = True
+
+        # Paste the characters on this Canvas.
+        for x in range(canvasToPaste.width):
+            for y in range(canvasToPaste.height):
+                if self.isOnCanvas(x + left, y + top):
+                    self._chars[x + left][y + top] = canvasToPaste._chars[x][y]
+                    self._fg[x + left][y + top] = canvasToPaste._fg[x][y]
+                    self._bg[x + left][y + top] = canvasToPaste._bg[x][y]
+                elif not truncate:
+                    raise PyTextCanvasException('Attempted to paste beyond the edge of the Canvas at x %s y %s' % (x + left, y + top))
+        self._strDirty = True
+
 
     def loads(self, content, truncate=True):
         r"""
